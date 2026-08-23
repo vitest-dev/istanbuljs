83e9ee74209fc412bacc51fdc539007562f00932
Ari Perkkiö <ari.perkkio@gmail.com>
2026-08-23 19:21:08 +0300

    feat!: module replacements

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "oxlint";
 
 export default defineConfig({
+  jsPlugins: ["@e18e/eslint-plugin"],
+
   rules: {
+    "e18e/ban-dependencies": "error",
+
     // TODO: Remove these and fix source code
     "no-unused-vars": "off",
     "unicorn/no-new-array": "off",
